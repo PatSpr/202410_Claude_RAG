4b51f52b69262996405f319c59161dc14ed966c6
SprePat <52283816+SprePat@users.noreply.github.com>
2024-10-13 17:36:42 +0200

    new commit

---
+++ Guide.py
@@ -0,0 +1,17 @@
+#1. Setup virtual environment
+    #python3 -m venv .venv
+    #source .venv/bin/activate
+    
+
+#2. Jupyter Notebooks Setup & Kernel setup/selection
+    #pip3 install jupyter
+    #pip3 install ipykernel
+    #python3 -m ipykernel install --user --name=202410ClaudeAPICall_kernel
+    #jupyter notebook
+    #Select kernel in jupyter notebook env / in iypnb file
+
+#4. Install packages from requirements file 
+
+
+
+#. 99_01 Saved 
